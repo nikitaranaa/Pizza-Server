22e1ff4d857c1a90e4f6ee7b2560d77360fe2931
Nikita Rana <91747922+nikitaranaa@users.noreply.github.com>
2023-09-05 10:28:03 +0530

    Update customerRoutes.js

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const {orderController, showOrder} = require('../controllers/customerControllers/orderController')
+const {orderController, showOrder, capturePayment, verifyPayment} = require('../controllers/customerControllers/orderController')
 const {fetchOrders} = require('../controllers/customerControllers/fetchOrderController')
 const {auth} = require('../middlewares/auth')
 
 
 router.post('/orders',auth,orderController)
+router.post('/orders/online',auth,capturePayment)
+router.post('/orders/paymentVerification',auth,verifyPayment)
 router.get('/:id',auth,showOrder)
 router.post('/orders-history',auth,fetchOrders)
 module.exports = router
